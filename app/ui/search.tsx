62cb5bf78a19bc5e8faf7117851ce6afccf1d1ee
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-06-29 01:07:46 -0400

    ok chapter 14

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,17 +1,22 @@
 'use client';
- 
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
- 
-export default function Search({ placeholder }: { placeholder: string }) {
+
+interface SearchProps {
+  placeholder: string;
+}
+
+export default function Search({ placeholder }: SearchProps) {
   const searchParams = useSearchParams();
-  const { replace } = useRouter();
   const pathname = usePathname();
- 
+  const { replace } = useRouter();
+
   const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+   
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -32,7 +37,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query') || ''}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
